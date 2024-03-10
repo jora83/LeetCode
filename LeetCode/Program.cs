e0173f44274986769ba91f25fa56c0baff010a03
alex jora <joraalex13@gmail.com>
2024-03-10 17:08:09 +0200

    Add project files.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,12 @@
+﻿bool ContainsDuplicate(int[] nums)
+{
+    HashSet<int> hasValues = new HashSet<int>();
+
+    foreach(int num in nums)
+    {
+        if (hasValues.Contains(num)) return true;
+        hasValues.Add(num);
+    }
+
+    return false;
+}
